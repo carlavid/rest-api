af37abb5d76474cdc28a3883a80218e18a11cdf6
Carla Vidales <carlavidales.ca@gmail.com>
2023-11-28 15:49:17 -0800

    Add route to create new user

--- routes.js
+++ routes.js
@@ -25,6 +25,28 @@ router.get(
 );
 
 // Route that creates a new user
+router.post(
+  "/users",
+  asyncHandler(async (req, res) => {
+    try {
+      await User.create(req.body);
+      // set location header
+      res.location("/");
+      // return 201 status code and no content
+      res.status(201).end();
+    } catch (error) {
+      if (
+        error.name === "SequelizeValidationError" ||
+        error.name === "SequelizeUniqueConstraintError"
+      ) {
+        const errors = error.errors.map((err) => err.message);
+        res.status(400).json({ errors });
+      } else {
+        throw error;
+      }
+    }
+  })
+);
 
 // Route that returns all users
 // router.get(
